474c67f2dd478122a9630be44934149c109b927f
Christa Tsao <tsao.christa@gmail.com>
2025-04-26 16:11:12 -0700

    Increasing Circle

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [radius, setRadius] = useState(20)
+
+  useEffect(() => {
+    setRadius((prev) => prev + count);
+  }, [count])
+
+  return (
+    <div onClick={() => { setCount((prev) => prev + 1); }} style={{ cursor: 'pointer', borderRadius: radius, width: radius, height: radius, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}><div style={{ color: 'black' }}>{count}</div></div>
+  )
+}
+
+export default App
